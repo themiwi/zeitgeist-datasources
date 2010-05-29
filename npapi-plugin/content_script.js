04929baaef1c825dd79af14869e56798d2013f62
Michal Hruby <michal.mhr@gmail.com>
2010-05-29 13:12:02 +0200

    Implement mime-type detection

--- npapi-plugin/content_script.js
+++ npapi-plugin/content_script.js
@@ -1,6 +1,39 @@
 var dict = {
 	"url": document.URL,
-	"mimeType": document.contentType,
 	"title": document.title
 };
-chrome.extension.sendRequest({name: "zgPlugin"}, dict);
+
+function updateContentType() {
+	var nodes = document.getElementsByTagName("meta");
+	for (var i=0; i<nodes.length; i++)
+	{
+		var node = nodes[i];
+		if (!node.hasAttributes()) continue;
+		var http_equiv = node.getAttribute("http-equiv");
+		if (http_equiv && http_equiv.toLowerCase() == "content-type")
+		{
+			var content_type = node.getAttribute("content");
+			if (!content_type) continue;
+			content_type = content_type.split(';')[0];
+			dict["mimeType"] = content_type;
+			return true;
+		}
+	}
+	return false;
+}
+
+if (updateContentType()) {
+	chrome.extension.sendRequest({name: "zgPlugin"}, dict);
+} else {
+	// send extra request to get the mime type
+	var = request = new XMLHttpRequest();
+	request.open("HEAD", document.URL, true);
+	request.onreadystatechange=function() {
+		if (request.readyState==4) {
+			var content = request.getResponseHeader("Content-Type");
+			dict["mimeType"] = content;
+			chrome.extension.sendRequest({name: "zgPlugin"}, dict);
+		}
+	}
+	request.send(null);
+}
