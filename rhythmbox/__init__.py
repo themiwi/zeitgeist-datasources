b4b9bdfc735cf078f1500c25c705239132e063c9
Michal Hruby <michal.mhr@gmail.com>
2010-10-18 15:26:11 +0200

    Relicense the Rhythmbox extension to GPL2 with Rhythmbox's exception to ease the inclusion process (license change was ACKed by all authors)

--- rhythmbox/__init__.py
+++ rhythmbox/__init__.py
@@ -1,22 +1,29 @@
 # -.- coding: utf-8 -.-
 
-# Zeitgeist
-#
 # Copyright © 2009 Markus Korn <thekorn@gmx.de>
 # Copyright © 2010 Laszlo Pandy <laszlok2@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# The Rhythmbox authors hereby grant permission for non-GPL compatible
+# GStreamer plugins to be used and distributed together with GStreamer
+# and Rhythmbox. This permission is above and beyond the permissions granted
+# by the GPL license by which Rhythmbox is covered. If you modify this code
+# you may extend this exception to your version of the code, but you are not
+# obligated to do so. If you do not wish to do so, delete this exception
+# statement from your version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA.
 
 import rb
 import rhythmdb
