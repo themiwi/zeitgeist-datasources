b6d07b96a7cb0edd031afdafaf84dff2f24fc766
Seif Lotfy <seif@lotfy.com>
2012-02-01 16:26:55 +0100

    Add proper handling of push events from websites such as chrome etc and only send event after the website has been visited for 5 seconds or more

--- chrome/zeitgeist.js
+++ chrome/zeitgeist.js
@@ -1,5 +1,6 @@
 var plugin = document.embeds[0];
 var tabInfo = {};
+var tabIdTimeouts = {};
 
 function onTabCreated (tab) {
 	chrome.tabs.executeScript(tab.id, {file: "content_script.js"});
@@ -11,7 +12,10 @@ function onTabRemoved (tabid) {
 
 function onTabUpdated (tabid, changeInfo, tab) {
 	if (!changeInfo.url) return;
-	chrome.tabs.executeScript(tabid, {file: "content_script.js"});
+	window.clearTimeout(tabIdTimeouts[tabid])
+ 	tabIdTimeouts[tabid] = window.setTimeout(function(){
+ 		chrome.tabs.executeScript(tabid, {file: "content_script.js"});},
+ 		5000);
 }
 
 function onBookmarkCreated (bookmarkid, bookmark) {
@@ -23,6 +27,7 @@ function onBookmarkCreated (bookmarkid, bookmark) {
 }
 
 function sendAccessEvent (documentInfo, tabid) {
+	console.log("Sending access event for " + documentInfo.url);
 	var url = documentInfo.url;
 	var origin = documentInfo.origin;
 	var domain = documentInfo.domain;
@@ -46,6 +51,7 @@ function sendAccessEvent (documentInfo, tabid) {
 function sendLeaveEvent (tabid) {
 	var documentInfo = tabInfo[tabid];
 	if (documentInfo == null || documentInfo.sentAccess != true) return;
+	console.log("Sending leave event for " + documentInfo.url);
 
 	var url = documentInfo.url;
 	var origin = documentInfo.origin;
@@ -70,8 +76,13 @@ function sendLeaveEvent (tabid) {
 // this works in chrome 7,8,9
 function onExtensionRequest (request, sender, sendResponse) {
 	var id = sender.tab.id;
-	sendLeaveEvent(id);
-	sendAccessEvent(request, id);
+	if (tabInfo[id] == undefined || tabInfo[id].url != request.url) {
+		/*if (tabInfo[id] != undefined) {
+			console.log("Leaving " + tabInfo[id].url);
+			sendLeaveEvent(id);
+		}*/
+		sendAccessEvent(request, id);
+	}
 }
 
 var is_chromium = /chromium/.test( navigator.userAgent.toLowerCase() );
@@ -81,7 +92,7 @@ else plugin.setActor("application://chromium-browser.desktop");
 chrome.extension.onRequest.addListener (onExtensionRequest);
 chrome.bookmarks.onCreated.addListener (onBookmarkCreated);
 chrome.tabs.onUpdated.addListener (onTabUpdated);
-chrome.tabs.onCreated.addListener (onTabCreated);
+//chrome.tabs.onCreated.addListener (onTabCreated);
 chrome.tabs.onRemoved.addListener (onTabRemoved);
 
 chrome.windows.getAll({"populate" : true}, function (windows) {
